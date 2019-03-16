f0904b9bd3f4fb2c4d8caec176b706555dba9c9e
toan <deptraimoitoinghien@gmail.com>
2019-03-16 21:37:19 +0700

    thay doi aaa.sql sang javaspring.sql

--- javaspring.sql
+++ javaspring.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 11, 2019 lúc 09:25 AM
--- Phiên bản máy phục vụ: 10.1.32-MariaDB
--- Phiên bản PHP: 7.2.5
+-- Thời gian đã tạo: Th3 16, 2019 lúc 03:35 PM
+-- Phiên bản máy phục vụ: 10.1.37-MariaDB
+-- Phiên bản PHP: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `javaspring`
+-- Cơ sở dữ liệu: `sbdb`
 --
 
 -- --------------------------------------------------------
@@ -30,11 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking` (
   `id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` smallint(6) NOT NULL,
+  `time` datetime NOT NULL,
   `total_price` float NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `status` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `user_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,11 +44,11 @@ CREATE TABLE `booking` (
 
 CREATE TABLE `booking_detail` (
   `id` int(11) NOT NULL,
-  `booking_id` int(11) NOT NULL,
-  `sp_id` int(11) NOT NULL,
   `duration` int(11) NOT NULL,
-  `expired_time` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `expired_time` date NOT NULL,
+  `booking_id` int(11) NOT NULL,
+  `sp_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,20 +59,7 @@ CREATE TABLE `booking_detail` (
 CREATE TABLE `extra_booking` (
   `id` int(11) NOT NULL,
   `detail_booking_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `roles`
---
-
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `role_name` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -82,28 +69,84 @@ CREATE TABLE `roles` (
 
 CREATE TABLE `sc_pitch` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `address` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `price` float NOT NULL,
-  `status` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `status` smallint(6) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `users`
+-- Cấu trúc bảng cho bảng `t_comment`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `address` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `t_comment` (
+  `id` bigint(20) NOT NULL,
+  `content` varchar(300) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `t_post`
+--
+
+CREATE TABLE `t_post` (
+  `id` bigint(20) NOT NULL,
+  `content` varchar(3000) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `title` varchar(300) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `t_role`
+--
+
+CREATE TABLE `t_role` (
+  `id` bigint(20) NOT NULL,
+  `role_name` varchar(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `t_role`
+--
+
+INSERT INTO `t_role` (`id`, `role_name`) VALUES
+(0, 'admin'),
+(1, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `t_user`
+--
+
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL,
+  `active` bit(1) NOT NULL,
+  `avatar_url` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `role_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `t_user`
+--
+
+INSERT INTO `t_user` (`id`, `active`, `avatar_url`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role_id`) VALUES
+(1, b'1', NULL, 'cuong@gmail.com', 'Cuong', 'Nguyen', '$2a$11$e1FqgTBdehuM6rTQZYFAEOQyoxHCY4KEl1K14O2uhHMy5BkFfyctu', '09434243232', 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -114,29 +157,22 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `fk_userid` (`user_id`);
 
 --
 -- Chỉ mục cho bảng `booking_detail`
 --
 ALTER TABLE `booking_detail`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `booking_id` (`booking_id`),
-  ADD KEY `sp_id` (`sp_id`);
+  ADD KEY `FK59941ondg9nwaifm2umnrduev` (`booking_id`),
+  ADD KEY `FKe726d13bqvuynlr4a2gi8pdh7` (`sp_id`);
 
 --
 -- Chỉ mục cho bảng `extra_booking`
 --
 ALTER TABLE `extra_booking`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `detail_booking_id` (`detail_booking_id`);
-
---
--- Chỉ mục cho bảng `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `FKal5lb6qrkp9mvsjjyxcel019r` (`detail_booking_id`);
 
 --
 -- Chỉ mục cho bảng `sc_pitch`
@@ -145,10 +181,33 @@ ALTER TABLE `sc_pitch`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `users`
+-- Chỉ mục cho bảng `t_comment`
 --
-ALTER TABLE `users`
+ALTER TABLE `t_comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKtamaoacctq4qpko6bvtv0ke1p` (`user_id`),
+  ADD KEY `FKsa3hl9a6mu30dct1jnn2bwvem` (`post_id`);
+
+--
+-- Chỉ mục cho bảng `t_post`
+--
+ALTER TABLE `t_post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK18f01t655nkcuptq9oxp04sgn` (`user_id`);
+
+--
+-- Chỉ mục cho bảng `t_role`
+--
+ALTER TABLE `t_role`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UK_i6qjjoe560mee5ajdg7v1o6mi` (`email`),
+  ADD KEY `FK76cs7cu4h4y8vc1vd4qyw36rt` (`role_id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -173,51 +232,10 @@ ALTER TABLE `extra_booking`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT cho bảng `sc_pitch`
 --
 ALTER TABLE `sc_pitch`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Các ràng buộc cho bảng `booking_detail`
---
-ALTER TABLE `booking_detail`
-  ADD CONSTRAINT `booking_detail_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`),
-  ADD CONSTRAINT `booking_detail_ibfk_2` FOREIGN KEY (`sp_id`) REFERENCES `sc_pitch` (`id`);
-
---
--- Các ràng buộc cho bảng `extra_booking`
---
-ALTER TABLE `extra_booking`
-  ADD CONSTRAINT `extra_booking_ibfk_1` FOREIGN KEY (`detail_booking_id`) REFERENCES `booking_detail` (`id`);
-
---
--- Các ràng buộc cho bảng `roles`
---
-ALTER TABLE `roles`
-  ADD CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

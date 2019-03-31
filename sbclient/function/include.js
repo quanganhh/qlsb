a84a238d827bdd1f84aac30d197df7720c3af844
DuySexy <hoangvanduy9x@gmail.com>
2019-03-31 22:58:13 +0700

    sua file index.html va include.js

--- sbclient/function/include.js
+++ sbclient/function/include.js
@@ -1,27 +1,28 @@
 
 /* hàm include file html dạng index.html?path=file-html */
-////function loadData() {
-//    var path = window.location.search;
-//    var file = path.split('=');
-//    if (typeof file[1] != 'undefined') {
-//        $('#body-data').load(file[1] + '.html');
-//    } else {
-//        $('#body-data').load('dashboard.html');
-//    }
-//}
+function loadData() {
+   var path = window.location.search;
+   var file = path.split('=');
+   if (typeof file[1] != 'undefined') {
+   	   var view = file[1].replace('-','/');
+       $('#body-data').load(view + '.html');
+   } else {
+       $('#body-data').load('dashboard.html');
+   }
+}
 
 /*  hàm gán link cho thẻ a , include k dùng <a href="link"></a> phải dùng <a onclick="path('đường dẫn file')"></a>*/
 function path(str) {
-    window.location.href = 'index.html#' + str;
+    window.location.href = 'index.html?view=' + str;
 }
 
 /* hàm include file html dạng index.html#file-html */
-function loadData() {
-    var path = window.location.hash;
-    var file = path.split('#');
-    if (typeof file[1] != 'undefined') {
-        $('#body-data').load(file[1] + '.html');
-    } else {
-        $('#body-data').load('dashboard.html');
-    }
-}
+// function loadData() {
+//     var path = window.location.hash;
+//     var file = path.split('#');
+//     if (typeof file[1] != 'undefined') {
+//         $('#body-data').load(file[1] + '.html');
+//     } else {
+//         $('#body-data').load('dashboard.html');
+//     }
+// }
